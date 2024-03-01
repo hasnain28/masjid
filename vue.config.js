d89ccc3e156c618a09867f5e0ecbccc683f8a789
Muhammad Hasnain Zaib <muhammadhasnain0334@gmail.com>
2024-03-01 19:16:36 +0500

    template

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    allowedHosts: ["masjid.mhasnain.dev", "masjid.test.mhasnain.dev"]
-  }
-})
